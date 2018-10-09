5e0d0eb48bc30c9ee160d5f9664d9518185cdfc1
Daré Adebanjo <dareofficial@element-x.lan>
2018-10-08 23:37:50 -0400

    main: fixed 'TableTest' line 7 name to match new name

--- src/test/java/core/TableTest.java
+++ src/test/java/core/TableTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestTable extends TestCase{
+public class TableTest extends TestCase{
 
 	public void testAddMeld() {	//Also tests numMelds()
 		Table table = new Table();
