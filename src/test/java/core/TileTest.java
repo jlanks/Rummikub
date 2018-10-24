6eee5c358fb598e29ac40e1909f4b4afee6560cd
Daré Adebanjo <dareofficial@Dares-Mac-Mini.local>
2018-10-24 14:47:37 -0400

    Modified the TileTest file

--- src/test/java/core/TileTest.java
+++ src/test/java/core/TileTest.java
@@ -13,6 +13,16 @@ public class TileTest extends TestCase {
 	}
 
 	/**
+	 * This tests: the tile constructor for string
+	 */
+	public void testTileString() {
+		// this confirms the tile was made
+		Tile tester = new Tile("r5");
+		assertNotNull(tester); // should be a success
+	}
+
+
+	/**
 	 * This tests/confirms: that the tile's color and value are set
 	 */
 	public void testCheckColorAndValue() {
