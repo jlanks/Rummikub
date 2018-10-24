44eb9b2a17280df71dd9e0685bd36d9199b9919c
Julian Lankstead <Julian@Julians-MacBook-Pro.local>
2018-10-24 10:52:26 -0400

    Meld: Constructor added to take in 4 tiles MeldTest: Tests added for the fourth constructor and pass

--- main/core/Meld.java
+++ main/core/Meld.java
@@ -6,30 +6,38 @@ public class Meld {
 	ArrayList<Tile> meld = new ArrayList<Tile>();
 
 	// *********** TILES NEED TO BE ADDED IN ORDER ************
+	
+	// Constructor for 3 tiles
 	Meld(Tile a, Tile b, Tile c, boolean first) {
 
 		// checking to see if the total for the first meld is 30 or higher
+		
 		if (first && a.getValue() + b.getValue() + c.getValue() < 30) {
 
-			// printing error to console
+			// printing error to console if error
 			System.out.println("TRING TO MAKE MELD TOTALED LESS THAN 30");
-
+			// returning due to invalid input
 			return;
 		}
 
 		else {
 
 			// checking to see if the colours are the same and numbers are valid
-			if (a.getColour() == b.getColour() && a.getColour() == c.getColour() && b.getColour() == c.getColour()
-					&& a.getValue() == b.getValue() + 1 && b.getValue() == c.getValue() + 1) {
+			// run identifier
+			if (	a.getColour() == b.getColour() && a.getColour() == c.getColour() 
+					&& b.getColour() == c.getColour()
+					&& a.getValue() == b.getValue() - 1 && 
+					b.getValue() == c.getValue() - 1) {
+				
 				// adding tiles to meld
 				meld.add(a);
 				meld.add(b);
 				meld.add(c);
 
-			}
+				}
 
 			// checking for a group of same number different colour
+			// set identifier
 			else if (a.getColour() != b.getColour() && a.getColour() != c.getColour() && b.getColour() != c.getColour()
 					&& a.getValue() == b.getValue() && b.getValue() == c.getValue() && a.getValue() == c.getValue()) {
 				// adding tiles to meld
@@ -41,6 +49,59 @@ public class Meld {
 
 		}
 	}
+	
+	// *********** TILES NEED TO BE ADDED IN ORDER ************ 
+		  // meld constructor for 4 tiles 
+		
+		Meld(Tile a, Tile b, Tile c, Tile d, boolean first) {
+			
+			// checking to see if the total for the first meld is 30 or higher
+			if(first && a.getValue() + b.getValue() + c.getValue() + d.getValue() < 30) {
+		  
+				// printing error to console
+				System.out.println("FAILED: TRYING TO MAKE MELD TOTALED LESS THAN 30");
+		  
+				return;
+		   
+			
+			}else {
+		  
+				// checking to see if the colours are the same and numbers are valid 
+				// checking if player is trying to make a run 
+				if( 	a.getColour() == b.getColour() && a.getColour() == c.getColour() &&
+						b.getColour() == c.getColour() && d.getColour() == a.getColour() &&
+						d.getColour() == b.getColour() && d.getColour() == c.getColour() &&
+						a.getValue() == b.getValue() - 1 && b.getValue() == c.getValue() - 1 &&
+						c.getValue() == d.getValue() -1 ) { 
+					
+					// adding tiles to meld. meld should be a run
+					
+					meld.add(a);
+					meld.add(b);
+					meld.add(c);
+					meld.add(d);
+		  
+				}
+		  
+				// checking for a group of same number different colour 
+				// checking if player is trying to make a set
+				else if(a.getColour()!= b.getColour() && a.getColour() != c.getColour() && b.getColour() !=
+						c.getColour() && d.getColour() != a.getColour() && d.getColour() !=
+						c.getColour() && a.getValue() == b.getValue() && b.getValue() == c.getValue()
+						&& a.getValue() == c.getValue() && d.getValue() == a.getValue()&&
+						d.getValue() == b.getValue() && d.getValue() == c.getValue()) { 
+						
+						// adding tiles to meld
+						// should be a set of 4 cards different colour same number
+				  		meld.add(a);
+				  		meld.add(b); 
+				  		meld.add(c); 
+				  		meld.add(d);
+		  
+				}
+		  
+			} 
+		}
 
 	public int getSum() {
 		int sum = 0;
@@ -49,41 +110,6 @@ public class Meld {
 		}
 		return sum;
 	}
-	/*
-	 * // *********** TILES NEED TO BE ADDED IN ORDER ************ // meld
-	 * constructor for 4 tiles private Meld(Tile a, Tile b, Tile c, Tile d, boolean
-	 * first) { // checking to see if the total for the first meld is 30 or higher
-	 * if(first && a.getValue() +b.getValue()+c.getValue() < 30) {
-	 * 
-	 * // printing error to console
-	 * System.out.println("TRING TO MAKE MELD TOTALED LESS THAN 30");
-	 * 
-	 * return; }
-	 * 
-	 * else {
-	 * 
-	 * // checking to see if the colours are the same and numbers are valid if(
-	 * a.getColour() == b.getColour() && a.getColour() == c.getColour() &&
-	 * b.getColour() == c.getColour() && d.getColour() == a.getColour() &&
-	 * d.getColour() == b.getColour() && d.getColour() == c.getColour() &&
-	 * a.getValue() == b.getValue() +1 && b.getValue() == c.getValue() + 1 &&
-	 * c.getValue() == d.getValue() +1 ) { // adding tiles to meld meld.add(a);
-	 * meld.add(b); meld.add(c); meld.add(d);
-	 * 
-	 * }
-	 * 
-	 * // checking for a group of same number different colour else if(a.getColour()
-	 * != b.getColour() && a.getColour() != c.getColour() && b.getColour() !=
-	 * c.getColour() && d.getColour() != a.getColour() && d.getColour() !=
-	 * c.getColour() && a.getValue() == b.getValue() && b.getValue() == c.getValue()
-	 * && a.getValue() == c.getValue() && d.getValue() == a.getValue()&&
-	 * d.getValue() == b.getValue() && d.getValue() == c.getValue()) { // adding
-	 * tiles to meld meld.add(a); meld.add(b); meld.add(c); meld.add(d);
-	 * 
-	 * }
-	 * 
-	 * 
-	 * 
-	 * } }
-	 */
+	
+	  
 }
--- src/test/java/core/MeldTest.java
+++ src/test/java/core/MeldTest.java
@@ -9,21 +9,77 @@ public class MeldTest extends TestCase {
 		// initializing
 		Deck MainDeck = new Deck();
 		Player Julian = new Player("Julian");
-		Meld NewMeld;
-		Tile t1, t2, t3;
+		Meld NewMeld, NewMeld2, NewMeld3, NewMeld4, NewMeld5, NewMeld6, NewMeld7, NewMeld8;
+		Tile t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15;
 
-		// adding tiles
+		// creating tiles to be used in melds
 		t1 = (new Tile(Colour.Red, 10));
 		t2 = (new Tile(Colour.Blue, 10));
 		t3 = (new Tile(Colour.Green, 10));
+		t4 = (new Tile(Colour.Orange, 10));
+		
+		
+		
+		t5 = (new Tile(Colour.Orange, 7));
+		t6 = (new Tile(Colour.Orange, 8));
+		t7 = (new Tile(Colour.Orange, 9));
+		t8 = (new Tile(Colour.Orange, 11));
 
-		// Making meld
-		NewMeld = new Meld(t1, t2, t3, false);
+		t9 = (new Tile(Colour.Red, 2));
+		t10 = (new Tile(Colour.Blue, 2));
+		t11= (new Tile(Colour.Green, 2));
+		t12= (new Tile(Colour.Orange, 2));
+		
+		t13 = (new Tile(Colour.Orange, 3));
+		t14 = (new Tile(Colour.Orange, 4));
+		t15 = (new Tile(Colour.Orange, 5));
+		
+		// Making meld with only three tiles as first play: set
+		NewMeld = new Meld(t1, t2, t3, true);
+		
+		// Making meld with only three tiles as normal play: set
+		NewMeld2 = new Meld(t9, t10, t11, false);
+		
+		// Making meld with only three tiles as first play: run 
+		NewMeld3 = new Meld(t7, t4, t8, true);
+		
+		// Making meld with only three tiles as normal play: run 
+		NewMeld4 = new Meld(t5, t6, t7, false);
+		
+		// Making meld with four tiles as first play: set 
+		NewMeld5 = new Meld(t1, t2, t3, t4, true);
+		
+		// Making meld with four tiles as normal play: set 
+		NewMeld6 = new Meld(t9, t10, t11, t12, false);
+		
+		// Making meld with four tiles as first play: run 
+		NewMeld7 = new Meld(t6, t7, t4, t8, true);
+		
+		// Making meld with four tiles as normal play: run
+		NewMeld8 = new Meld(t12, t13, t14, t15, false);
+		
+		
 
-		// making sure invalid tiles are not able to be added
+		// checking set melds of 3 tiles 
+	
 		assertEquals(30, NewMeld.getSum());
-		// making sure valid tiles are added
-		// assertEquals(true,NewMeld.addTile(new Tile(O,10)));
+		assertEquals(6, NewMeld2.getSum());
+	
+		// checking run melds of 3 tiles
+		
+		assertEquals(30, NewMeld3.getSum());
+		assertEquals(24, NewMeld4.getSum());
+		
+		// checking set melds of 4 tiles 
+		
+		assertEquals(40, NewMeld5.getSum());
+		assertEquals(8, NewMeld6.getSum());
+		
+		// checking run melds of 4 tiles 
+		
+		assertEquals(38, NewMeld7.getSum());
+		assertEquals(14, NewMeld8.getSum());
+		
 	}
 	/*
 	 * public void testRemoveFromMeld() {
