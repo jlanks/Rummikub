2520673aab8a6b3df448b3cee938a47b58fe5eaa
Noah Beeney <noahbeeney@hotmail.com>
2018-10-28 16:12:37 -0400

    Tile: create Tile with string, toString method, equals method.

--- main/core/Tile.java
+++ main/core/Tile.java
@@ -21,34 +21,59 @@ public class Tile {
 		TColour = colour;
 	}
 
-/*
 	// Tile constructor
 	public Tile(String input) {
 
-
-		// bounds checking
-		if (input.charAt(1) > 13 || input.charAt(1) < 1) {
-
-			System.err.println("Enter a valid card number");
-			System.exit(1);
-
+		//tile color
+		String strCol = Character.toString(input.charAt(0)).toUpperCase();
+		
+		if(strCol.equals("R")) {
+			TColour = Colour.Red;
+		} else if(strCol.equals("G")) {
+			TColour = Colour.Green;
+		} else if(strCol.equals("B")) {
+			TColour = Colour.Blue;
+		} else if(strCol.equals("O")) {
+			TColour = Colour.Orange;
 		}
-
-		// number assignment
-		number = input.charAt(1);
-		// Suit assignment
-		TColour = input.charAt(0);
+		
+		//tile value
+		number = Integer.parseInt(input.substring(1));
 	}
 
-*/
+
 	// basic getters
 	public Colour getColour() {
-
 		return this.TColour;
 	}
 
 	public int getValue() {
-
 		return this.number;
+	}
+	
+	public String toString() {
+		String output = "";
+		
+		if(this.TColour == Colour.Red) {
+			output += "R";
+		} else if(this.TColour == Colour.Green) {
+			output += "G";
+		} else if(this.TColour == Colour.Blue) {
+			output += "B";
+		} else if(this.TColour == Colour.Orange) {
+			output += "O";
+		}
+		
+		output += Integer.toString(this.number);
+		
+		return output;
+	}
+	
+	public boolean equals(Tile t) {
+		if(this.TColour == t.TColour && this.number == t.number) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
