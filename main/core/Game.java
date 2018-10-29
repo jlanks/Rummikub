51f9fc1bc327a05d21ed256dae99f7a7b20add87
Julian Lankstead <Julian@Julians-MacBook-Pro.local>
2018-10-29 16:44:40 -0400

    Game: basic constructor implemeted GameTest: Junit tests for checkwinner added

---
+++ main/core/Game.java
@@ -0,0 +1,17 @@
+package core;
+
+public class Game {
+	private Table table; 
+	
+	public Game() {
+		
+		table = new Table(); 
+	
+	}
+	
+	public Table getTable() {
+		
+		return table; 
+	}
+
+}
--- src/test/java/core/GameTest.java
+++ src/test/java/core/GameTest.java
@@ -6,24 +6,30 @@ import org.junit.Test;
 
 public class GameTest {
 
-	
-	public void testConstructor() {
-		// creating a game
+	@Test
+	public void testinit() {
+		
+			// creating a game
+			Game gameone = new Game(); 
+			
+			// making sure a player on a table is accessible 
+			assertEquals("Player 1",gameone.getTable().player1().getName()); 
+			
+			// making sure a player has a full hand after init
+			gameone.getTable().init();
+			assertEquals(14,gameone.getTable().player1().getHandSize());
+		
+	}
+	public void testCheckWinner() {
+		
 		Game gameone = new Game(); 
 		
-		// making sure a player on a table is accessible 
-		assertEquals("Player1",gameone.getTable().PlayerOne().getName()); 
+		// hand has not yet been delt. since the winning condition is 
+		// to have zero tiles checkwinner should return true
 		
-		// making sure a player has a full hand
-		assertEquals(14,gameone.getTable().PlayerOne().getHandSize());
-		
+		assertEquals(true,gameone.getTable().player1().CheckWinner()); 		
+	
 		
 		
 	}
-	
-	
-	
-
-	
-
 }
