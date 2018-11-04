7019eda77b8cc51fc6451503ab1f2bb7b04fdc72
Noah Beeney <noahbeeney@hotmail.com>
2018-11-04 13:10:05 -0500

    GameTest: added tests for Game class

--- src/test/java/core/GameTest.java
+++ src/test/java/core/GameTest.java
@@ -8,81 +8,357 @@ public class GameTest {
 
 	@Test
 	public void testinit() {
-
-		// creating a game
-		Game gameone = new Game();
-
-		// making sure a player on a table is accessible
-		assertEquals("Player 1", gameone.getTable().player1().getName());
-
-		// initializing the table for the game
-		gameone.getTable().init();
-		
-		// making sure a player has a full hand after init
-		assertEquals(14, gameone.getTable().player1().getHandSize());
-
-	}
-	
-	@Test
-	public void testCheckWinner() {
-		
-		// creating a game
-		Game gameone = new Game();
-
-		// hand has not yet been delt. since the winning condition is
-		// to have zero tiles checkwinner should return true
-
-		// testing a table with a player with zero tiles
-		assertEquals(true, gameone.getTable().player1().checkWinner());
-		
-		// adding cards into the picture
-		gameone.getTable().init();
-		
-		// testing an game with no current winner returns false
-		assertEquals(false, gameone.getTable().player1().checkWinner());
-
-	}
-	
-	@Test
-	public void testNextPlayer() {
-		
-		// creating a game
-		Game gameOne = new Game(); 
-		
-		// tests p1 is default starting player
-		assertEquals("Player 1",gameOne.getPlayer().getName()); 
-		
-		// next turn, currPlayer is switched to p2
-		gameOne.nextTurn();
-		// making sure switch from p1 - p2 was successful 
-		assertEquals("Player 2",gameOne.getPlayer().getName()); 
-		
-		// next turn, currPlayer is switched to p3
-		gameOne.nextTurn();
-		// making sure switch from p2 - p3 was successful 
-		assertEquals("Player 3",gameOne.getPlayer().getName()); 
-		
-		// next turn, currPlayer is switched to p1
-		gameOne.nextTurn();
-		// making sure switch from p3 - p1 was successful 
-		assertEquals("Player 1",gameOne.getPlayer().getName()); 
-		
-	}
-	public void testPlaying() {
-		// making game
 		Game game = new Game();
-		
-		// initializing table
-		game.getTable().init();
-		
-		// testing a player can play their turn
-		//assertEquals(0,game.getPlayer().Play(game)); 
-		
-		game.getPlayer().Play(game); 
-		
-		// testing a player can add a meld to a table
-		assertEquals(1,game.getTable().numMelds()); 
-		
-		
+		Table table = game.getTable();
+		Player pHuman = table.player1();
+		Player p1 = table.player2();
+		Player p3 = table.player3();
+
+		assertEquals("Human Player", pHuman.getName()); // StrategyHuman
+		assertEquals("Player 1", p1.getName()); // StrategyOne
+		assertEquals("Player 3", p3.getName()); // StrategyTwo
+
+		assertEquals(game.getPlayer(), pHuman);
+		game.nextTurn();
+		assertEquals(game.getPlayer(), p1);
+		game.nextTurn();
+		assertEquals(game.getPlayer(), p3);
+		game.nextTurn();
+		assertEquals(game.getPlayer(), pHuman);
+
+		game.end();
+	}
+
+	@Test
+	public void testInitialSingleMeld() {
+		Game game = new Game();
+		Table table = game.getTable();
+		Player pHuman = table.player1();
+		Player p1 = table.player2();
+		Player p3 = table.player3();
+
+		assertEquals(0, p1.getHandSize());
+		assertEquals(0, p3.getHandSize());
+
+		pHuman.addTile(new Tile("R1"));// dummy tile so human doens't win
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("G11"));
+
+		assertEquals(2, p1.getHandSize());
+		assertEquals(2, p3.getHandSize());
+
+		// Neither player can play yet
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays, draws tile
+		game.nextTurn();
+		game.getPlayer().Play(game); // p3 plays, draws tile
+		game.nextTurn();
+
+		assertEquals(3, p1.getHandSize());
+		assertEquals(3, p3.getHandSize());
+
+		assertEquals(0, table.numMelds());
+
+		p1.addTile(new Tile("R11"));
+		p3.addTile(new Tile("G12"));
+
+		// Both players should play their initial melds worth 30 and 33 respectively
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays
+
+		assertEquals(1, table.numMelds());
+
+		game.nextTurn();
+		game.getPlayer().Play(game); // p3 plays
+
+		assertEquals(2, table.numMelds());
+
+		game.end();
+	}
+
+	public void testInitialMultiMeld() {
+		Game game = new Game();
+		Table table = game.getTable();
+		Player pHuman = table.player1();
+		Player p1 = table.player2();
+		Player p3 = table.player3();
+
+		pHuman.addTile(new Tile("R1"));// dummy tile so human doesn't win
+
+		p1.addTile(new Tile("R4"));
+		p1.addTile(new Tile("R5"));
+		p1.addTile(new Tile("R6"));
+		p3.addTile(new Tile("G5"));
+		p3.addTile(new Tile("G6"));
+		p3.addTile(new Tile("G7"));
+
+		// Neither player can play yet
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays, draws tile
+		game.nextTurn();
+		game.getPlayer().Play(game); // p3 plays, draws tile
+		game.nextTurn();
+
+		assertEquals(0, table.numMelds());
+
+		p1.addTile(new Tile("B4"));
+		p1.addTile(new Tile("B5"));
+		p1.addTile(new Tile("B6"));
+		p3.addTile(new Tile("O5"));
+		p3.addTile(new Tile("O6"));
+		p3.addTile(new Tile("O7"));
+
+		// Both players should play their initial two melds worth total of 30 and 36
+		// respectively
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays
+
+		assertEquals(2, table.numMelds());
+
+		game.nextTurn();
+		game.getPlayer().Play(game); // p3 plays
+
+		assertEquals(4, table.numMelds());
+
+		game.end();
+	}
+
+	public void testMeldCombos() {
+		Game game = new Game();
+		Table table = game.getTable();
+		Player pHuman = table.player1();
+		Player p1 = table.player2();
+		Player p3 = table.player3();
+
+		// dummy tiles to prevent players from winning
+		pHuman.addTile(new Tile("R1"));
+		p1.addTile(new Tile("R1"));
+		p3.addTile(new Tile("R1"));
+
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("R11"));
+		p3.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("B10"));
+
+		assertEquals(0, table.numMelds());
+
+		// Both players play initial 30
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays single run
+		game.nextTurn();
+
+		game.show();
+		assertEquals(1, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 plays single set
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("R11"));
+		p1.addTile(new Tile("G9"));
+		p1.addTile(new Tile("G10"));
+		p1.addTile(new Tile("G11"));
+
+		p3.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("B10"));
+		p3.addTile(new Tile("R11"));
+		p3.addTile(new Tile("G11"));
+		p3.addTile(new Tile("B11"));
+
+		// Both players' second turn
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays multiple runs
+		game.nextTurn();
+
+		game.show();
+		assertEquals(4, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 plays multiple sets
+		game.nextTurn();
+
+		game.show();
+		assertEquals(6, table.numMelds());
+
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("R11"));
+		p3.addTile(new Tile("G9"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("G11"));
+
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("G10"));
+		p1.addTile(new Tile("B10"));
+		p3.addTile(new Tile("R11"));
+		p3.addTile(new Tile("G11"));
+		p3.addTile(new Tile("B11"));
+
+		// Both players' third turn
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays set and meld
+		game.nextTurn();
+
+		game.show();
+		assertEquals(8, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 plays set and meld
+		game.nextTurn();
+
+		game.show();
+		assertEquals(10, table.numMelds());
+
+		game.end();
+	}
+
+	public void testStrategies() {
+		Game game = new Game();
+		Table table = game.getTable();
+		Player pHuman = table.player1();
+		Player p1 = table.player2();
+		Player p3 = table.player3();
+
+		// dummy tiles to prevent players from winning
+		pHuman.addTile(new Tile("R1"));
+		p1.addTile(new Tile("R1"));
+		p3.addTile(new Tile("R1"));
+
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("R11"));
+		p3.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("B10"));
+
+		game.show();
+		assertEquals(0, table.numMelds());
+
+		// Both players play initial 30
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays single run
+		game.nextTurn();
+
+		game.show();
+		assertEquals(1, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 plays single set
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		// p1 gets useless tiles
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("O11"));
+		p1.addTile(new Tile("G9"));
+		p1.addTile(new Tile("G10"));
+		p1.addTile(new Tile("O11"));
+
+		p3.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("B10"));
+		p3.addTile(new Tile("R11"));
+		p3.addTile(new Tile("G11"));
+		p3.addTile(new Tile("B11"));
+
+		// Both players' second turn
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 cannot play, draws tile
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 can play, but does not (p1 does not have 3 fewer tiles)
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		game.end();
+
+		/*** TEST P3 PLAYS ALL THE TILES THEY CAN, IF THEY CAN WIN ***/
+		game = new Game();
+		table = game.getTable();
+		pHuman = table.player1();
+		p1 = table.player2();
+		p3 = table.player3();
+
+		// dummy tiles to prevent players from winning
+		pHuman.addTile(new Tile("R1"));
+		p1.addTile(new Tile("R1"));
+		p3.addTile(new Tile("R1"));
+
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("R11"));
+		p3.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("B10"));
+
+		game.show();
+		assertEquals(0, table.numMelds());
+
+		// Both players play initial 30
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 plays single run
+		game.nextTurn();
+
+		game.show();
+		assertEquals(1, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 plays single set
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		// p1 gets useless tiles
+		p1.addTile(new Tile("R9"));
+		p1.addTile(new Tile("R10"));
+		p1.addTile(new Tile("O11"));
+		p1.addTile(new Tile("G9"));
+		p1.addTile(new Tile("G10"));
+		p1.addTile(new Tile("O11"));
+		p1.addTile(new Tile("R4"));
+		p1.addTile(new Tile("R6"));
+
+		p3.addTile(new Tile("R10"));
+		p3.addTile(new Tile("G10"));
+		p3.addTile(new Tile("B10"));
+		p3.addTile(new Tile("R11"));
+		p3.addTile(new Tile("G11"));
+		p3.addTile(new Tile("B11"));
+
+		// Give p3 tiles to make meld with dummy "R1" tile
+		p3.addTile(new Tile("R2"));
+		p3.addTile(new Tile("R3"));
+
+		// Both players' second turn
+		game.nextTurn();// skip pHuman
+		game.getPlayer().Play(game); // p1 cannot play, draws tile
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		game.getPlayer().Play(game); // p3 can win so plays all tiles (even though p1 does not have 3 fewer tiles)
+		game.nextTurn();
+
+		game.show();
+		assertEquals(2, table.numMelds());
+
+		assertEquals(true, game.hasWinner());
+
+		game.end();
 	}
 }
