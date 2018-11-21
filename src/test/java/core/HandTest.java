6e597a6ea9b319d205af5d9eb17a7eca7c38b278
Julian Lankstead <Julian@Julians-MacBook-Pro.local>
2018-11-21 12:54:57 -0500

    HandTest: test case added for finding the sum of the runs in the current players hand

--- src/test/java/core/HandTest.java
+++ src/test/java/core/HandTest.java
@@ -181,5 +181,38 @@ public class HandTest {
 		
 		
 	}
+	@Test
+	public void testMeldInHandSum() {
+		// make a hand 
+		Hand hand = new Hand(); 
+		
+		
+		// testing constructor worked
+		assertEquals(hand.getSize(),0); 
+		// making tiles 
+		Tile t5,t6,t7,t8,t9,t10,t11,t12,t13;
+		t5 = (new Tile(Colour.Orange, 7));
+		t6 = (new Tile(Colour.Orange, 8));
+		t7 = (new Tile(Colour.Orange, 9));
+		t8 = (new Tile(Colour.Orange, 10));
+		t9 = (new Tile(Colour.Orange, 11));
+		t10 = (new Tile(Colour.Orange, 12));
+		t11 = (new Tile(Colour.Orange, 13));
+		t12 = (new Tile(Colour.Green, 13));
+		t13 = (new Tile(Colour.Blue, 13));
+		// *******************************************8
+		Game game = new Game(); 
+		// testing constructor worked
+		assertEquals(hand.getSize(),0); 
+		//adding tiles to make a run of size 3 
+		hand.add(t5); 
+		hand.add(t6); 
+		hand.add(t7); 
+		//testing the sum of get runsum is 24
+		assertEquals(hand.getRunSum(),24); 
+		
+		
+		
+	}
 
 }
